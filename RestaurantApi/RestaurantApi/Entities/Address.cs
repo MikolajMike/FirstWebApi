55e312198e12e95563c4b52e7aea92ba3191b7dd
MikolajMike <100801660+MikolajMike@users.noreply.github.com>
2022-09-27 19:43:27 +0200

    Added database.

---
+++ RestaurantApi/RestaurantApi/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantApi.Entities
+{
+    public class Address
+    {
+        public int id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+
+        public virtual Restaurant Restaurant { get; set; }
+
+    }
+}
---
+++ RestaurantApi/RestaurantApi/Entities/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool HasDevilery { get; set; }
+        public string Contactemail { get; set; }
+        public string ContactNumber { get; set; }
+
+        public int AddressId { get; set; }
+        public virtual Address Addres { get; set; }
+
+        public virtual List<Dish> Dishes { get; set; }
+    }
+}
